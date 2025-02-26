9771cc2c70ffb8f4b87334c0e6e1ccf20228fd8a
Websevee <websevee@gmail.com>
2025-02-26 03:35:43 +0300

    qwe

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,17 +3,8 @@
 # Установка Go-зависимостей
 go mod tidy
 
-# Создание пользователя и базы данных
-echo "1"
-
-# ! PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_NAME
-
-# export PGPASSWORD=val1dat0r
-psql -h localhost -p 5432 -U postgres -c "CREATE DATABASE \"project-sem-1\" OWNER validator;"
-
-echo "2"
-
-psql "postgresql://validator:val1dat0r@localhost:5432/project-sem-1" -c "
+# Создание таблицы
+psql -h localhost -p 5432 -U validator -d project-sem-1 -c "
 CREATE TABLE IF NOT EXISTS prices (
     id SERIAL PRIMARY KEY,
     created_at DATE,
