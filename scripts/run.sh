4ec2b949a85d3afdf66ba6cf529272d843245540
Websevee <websevee@gmail.com>
2025-02-26 20:44:42 +0300

    Test 2

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-
 go run cmd/server/main.go &
-
-# sleep 5
 
 # Сохраняем PID процесса сервера
 SERVER_PID=$!
 
+while ! nc -z localhost 8080; do
+    sleep 1
+done
+
 # Выводим PID для информации
 echo "Сервер запущен с PID: $SERVER_PID"
-
-# Завершаем скрипт, но оставляем сервер работать
-exit 0
