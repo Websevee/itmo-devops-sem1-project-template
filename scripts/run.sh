9a397ccad336ba6929bf51519af23cdef333954c
Websevee <websevee@gmail.com>
2025-02-26 20:39:10 +0300

    Test

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-go run cmd/server/main.go &
+go run cmd/server/main.go 
+# go run cmd/server/main.go &
 
+# # sleep 5
+
+# # Сохраняем PID процесса сервера
+# SERVER_PID=$!
 # sleep 5
 
-# Сохраняем PID процесса сервера
-SERVER_PID=$!
-sleep 5
+# # Выводим PID для информации
+# echo "Сервер запущен с PID: $SERVER_PID"
 
-# Выводим PID для информации
-echo "Сервер запущен с PID: $SERVER_PID"
-
-# Завершаем скрипт, но оставляем сервер работать
-exit 0
+# # Завершаем скрипт, но оставляем сервер работать
+# exit 0
