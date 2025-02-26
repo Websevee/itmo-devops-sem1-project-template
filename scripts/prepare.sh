b985108f70051aa99a456118227291e6b2590882
Websevee <websevee@gmail.com>
2025-02-26 03:39:37 +0300

    qwe3

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -4,8 +4,7 @@
 go mod tidy
 
 # Создание таблицы
-PGPASSWORD=val1dat0r
-psql -h localhost -p 5432 -U validator -d project-sem-1 -c "
+PGPASSWORD=val1dat0r psql -h localhost -p 5432 -U validator -d project-sem-1 -c "
 CREATE TABLE IF NOT EXISTS prices (
     id SERIAL PRIMARY KEY,
     created_at DATE,
