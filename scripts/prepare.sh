572ec63286ef8ea9946dd7f8cd172ecbbe4a50ab
Websevee <websevee@gmail.com>
2025-02-26 03:08:11 +0300

    Psql

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -7,9 +7,7 @@ done
 echo "PostgreSQL доступен."
 
 # Создание пользователя и базы данных
-sudo -u postgres psql -h localhost -p 5432 -c "CREATE USER validator WITH PASSWORD 'val1dat0r';"
-sudo -u postgres psql -h localhost -p 5432 -c "CREATE DATABASE \"project-sem-1\" OWNER validator;"
-
+psql -c "CREATE DATABASE \"project-sem-1\" OWNER validator;"
 psql "postgresql://validator:val1dat0r@localhost:5432/project-sem-1" -c "
 CREATE TABLE IF NOT EXISTS prices (
     id SERIAL PRIMARY KEY,
